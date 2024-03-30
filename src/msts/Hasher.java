18b3022d96d34560af0b73a5b9214d9fff438133
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-03-30 22:52:15 +0800

    changes, remove unused hashing function

--- src/msts/Hasher.java
+++ src/msts/Hasher.java
@@ -6,16 +6,6 @@ import java.util.UUID;
 
 public class Hasher {
 
-    public static String sha256( byte[] blockBytes ) {
-        try {
-            byte[] hashBytes = MessageDigest.getInstance("SHA-256").digest( blockBytes );
-            return Base64.getEncoder().encodeToString( hashBytes );
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     private static String hash(String input, String algorithm) {
         MessageDigest md;
         try
@@ -32,46 +22,12 @@ public class Hasher {
         }
     }
 
-    private static String saltHash(String input, byte[] salt, String algorithm) {
-        MessageDigest md;
-        try
-        {
-            md = MessageDigest.getInstance(algorithm);
-            md.update( input.getBytes() );
-            md.update( salt );
-
-            byte[] hashBytes = md.digest();
-            return Base64.getEncoder().encodeToString( hashBytes );
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     public static String md5(String input) {
         return hash(input, "MD5");
     }
 
-    public static String rsa(String input) {
-        return hash(input, "RSA");
-    }
-
-    public static String rsa(String input, byte[] salt) {
-        return saltHash(input, salt, "RSA");
-    }
-
     public static String sha256(String input) {
         return hash(input, "SHA-256");
-    }
-
-    public static String sha256(String input, byte[] salt) {
-        return saltHash(input, salt, "SHA-256");
-    }
-
-    public static String sha256Salt(String input) {
-        byte[] salt = Salt.generateSalt();
-        return saltHash(input, salt, "SHA-256");
     }
 
     public static String generateUUID(String input) { return UUID.nameUUIDFromBytes(input.getBytes()).toString(); }
